6487f44d64de37ac4854b36306d4028ad3fc5ac2
Parth <parth@mehrotra.me>
2023-11-19 23:32:18 -0500

    add support for macOS

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,17 @@ pub struct PdfiumWrapper {
 }
 
 impl PdfiumWrapper {
+    #[cfg(target_os = "macos")]
+    pub fn init(binary_path: &String) {
+        let lib_bytes = include_bytes!("../pdfium-macos/lib/libpdfium.dylib");
+
+        fs::create_dir_all(Path::new(&binary_path).parent().unwrap()).unwrap();
+        let binary_path = format!("{}/pdfium.dylib", binary_path);
+        println!("{:#?}", binary_path);
+        //make sure the path is valid
+        fs::write(binary_path, lib_bytes).unwrap();
+    }
+
     #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
     pub fn init(binary_path: &String) {
         let lib_bytes = include_bytes!("../pdfium-windows/x64/lib/pdfium.dll");
