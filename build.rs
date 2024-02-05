7fa55f35513fd5e20d7f3310c6edca13a94f8f02
Parth <parth@mehrotra.me>
2024-02-05 18:33:21 -0500

    fix build scripts

--- build.rs
+++ build.rs
@@ -1,17 +1,34 @@
-#[cfg(target_os = "macos")]
 fn main() {
     let mut current_dir = std::env::current_dir().unwrap();
-    current_dir.push("pdfium-macos");
+
+    if macos() {
+        current_dir.push("pdfium-macos");
+    } else if ios() {
+        current_dir.push("pdfium-ios");
+    } else if ios_sim() {
+        current_dir.push("pdfium-ios-sim");
+    } else if android() {
+        todo!("@smail get android static lib from: https://github.com/paulocoutinhox/pdfium-lib")
+    }
+
     let current_dir = current_dir.display();
+
     println!("cargo:rustc-link-lib=static=pdfium");
     println!("cargo:rustc-link-search=native={current_dir}");
 }
 
-#[cfg(target_os = "ios")]
-fn main() {
-    let mut current_dir = std::env::current_dir().unwrap();
-    current_dir.push("pdfium-macos");
-    let current_dir = current_dir.display();
-    println!("cargo:rustc-link-lib=static=pdfium");
-    println!("cargo:rustc-link-search=native={current_dir}");
+fn macos() -> bool {
+    std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "macos"
+}
+
+fn ios() -> bool {
+    std::env::var("TARGET").unwrap() == "aarch64-apple-ios"
+}
+
+fn ios_sim() -> bool {
+    std::env::var("TARGET").unwrap() == "aarch64-apple-ios-sim"
+}
+
+fn android() -> bool {
+    std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "android"
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub fn init(_statically_linked: &String) -> Pdfium {
 }
 
 #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
-pub fn init(binary_path: &String) {
+pub fn init(binary_path: &String) -> Pdfium {
     use std::{fs, path::Path};
 
     let lib_bytes = include_bytes!("../pdfium-windows/x64/lib/pdfium.dll");
@@ -19,10 +19,18 @@ pub fn init(binary_path: &String) {
     println!("{:#?}", binary_path);
     //make sure the path is valid
     fs::write(binary_path, lib_bytes).unwrap();
+
+    let bindings = Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
+        &binary_path,
+    ))
+        .unwrap();
+
+    Pdfium::new(bindings)
 }
 
 #[cfg(all(target_os = "windows", target_arch = "aarch64"))]
-pub fn init(binary_path: &String) {
+pub fn init(binary_path: &String) -> Pdfium {
+    use std::{fs, path::Path};
     let lib_bytes = include_bytes!("../pdfium-windows/arm64/lib/pdfium.dll");
 
     fs::create_dir_all(Path::new(&binary_path).parent().unwrap()).unwrap();
@@ -30,15 +38,31 @@ pub fn init(binary_path: &String) {
     println!("{:#?}", binary_path);
     //make sure the path is valid
     fs::write(binary_path, lib_bytes).unwrap();
+
+    let bindings = Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
+        &binary_path,
+    ))
+        .unwrap();
+
+    Pdfium::new(bindings)
 }
 
 #[cfg(target_os = "linux")]
-pub fn init(binary_path: &String) {
+pub fn init(binary_path: &String) -> Pdfium {
+    use std::{fs, path::Path};
+
     let lib_bytes = include_bytes!("../pdfium-linux/lib/libpdfium.so");
 
     fs::create_dir_all(Path::new(&binary_path).parent().unwrap()).unwrap();
     let binary_path = format!("{}/libpdfium.so", binary_path);
     //make sure the path is valid
     fs::write(binary_path, lib_bytes).unwrap();
+
+    let bindings = Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
+        &binary_path,
+    ))
+        .unwrap();
+
+    Pdfium::new(bindings)
 }
 
