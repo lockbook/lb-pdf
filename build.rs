445ada45289c612b06e2eade6bc125b1c10a1f6d
Parth <parth@mehrotra.me>
2024-02-05 20:35:02 -0500

    fix build on linux

--- build.rs
+++ build.rs
@@ -1,20 +1,24 @@
 fn main() {
-    let mut current_dir = std::env::current_dir().unwrap();
+    if should_static_link() {
+        let mut current_dir = std::env::current_dir().unwrap();
 
-    if macos() {
-        current_dir.push("pdfium-macos");
-    } else if ios() {
-        current_dir.push("pdfium-ios");
-    } else if ios_sim() {
-        current_dir.push("pdfium-ios-sim");
-    } else if android() {
-        todo!("@smail get android static lib from: https://github.com/paulocoutinhox/pdfium-lib")
+        if macos() {
+            current_dir.push("pdfium-macos");
+        } else if ios() {
+            current_dir.push("pdfium-ios");
+        } else if ios_sim() {
+            current_dir.push("pdfium-ios-sim");
+        } else if android() {
+            todo!(
+                "@smail get android static lib from: https://github.com/paulocoutinhox/pdfium-lib"
+            )
+        }
+
+        let current_dir = current_dir.display();
+
+        println!("cargo:rustc-link-lib=static=pdfium");
+        println!("cargo:rustc-link-search=native={current_dir}");
     }
-
-    let current_dir = current_dir.display();
-
-    println!("cargo:rustc-link-lib=static=pdfium");
-    println!("cargo:rustc-link-search=native={current_dir}");
 }
 
 fn macos() -> bool {
@@ -31,4 +35,12 @@ fn ios_sim() -> bool {
 
 fn android() -> bool {
     std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "android"
+}
+
+fn apple() -> bool {
+    std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap() == "apple"
+}
+
+fn should_static_link() -> bool {
+    android() || apple()
 }
